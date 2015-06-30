4f64d3516274fbccd0b6c9d102b9bb6013629f4a
k-a-webb <kristiannewebb@gmail.com>
2015-06-30 10:14:16 -0300

    working python imcombine, headers still to do

--- IFU_reduction_wl/do_all.py
+++ IFU_reduction_wl/do_all.py
@@ -12,6 +12,7 @@ import os
 from scipy import ndimage
 from ppxf import ppxf
 import ppxf_util as util
+import glob
 
 """
 This script covers the necessary steps to preform the pPXF kinematic analysis of a given 3D image cube.
@@ -35,12 +36,14 @@ VEL_INIT = 1500
 SIG_INIT = 100.
 XAXIS_HEADER = 'NAXIS1'
 YAXIS_HEADER = 'NAXIS2'
+XDIM_HEADER = 'CD1_1'
+YDIM_HEADER = 'CD2_2'
 LAM_RANGE = [4186, 5369]
 TEMPLATE_PATH = '/Users/kwebb/idl/cappellari/ppxf/spectra/'
 TEMPLATE_FITS = 'Mun1.30z*.fits'  # Make sure to change sigma in pPXF if change template
 
 # These variables define the chosen file structure I use to organise the output
-PROC_PATH = 'pre_ppxf_proc'
+PROC_PATH = 'ppxf_proc'
 if not os.path.exists(os.path.join(DIR_PATH, PROC_PATH)):
     os.mkdir(os.path.join(DIR_PATH, PROC_PATH))
 XYSN_FILE = os.path.join(DIR_PATH, PROC_PATH, 'x_y_signal_noise.txt')
@@ -55,14 +58,15 @@ FILE_VAR = 'imcomb_var_{}.lis'
 BIN_SCI = 'bin_sci_{}.fits'
 BIN_VAR = 'bin_var_{}.fits'
 IN_FILE_PREFIX = 'bin_sci_*'
-PPXF_PROC_PATH = 'ppxf'
+PPXF_PROC_PATH = 'ppxf_output'
 OUT_VEL_SIGMA = os.path.join(DIR_PATH, PROC_PATH, PPXF_PROC_PATH, 'vel_sigma_output_sn30.txt')
 OUT_VEL = os.path.join(DIR_PATH, PROC_PATH, PPXF_PROC_PATH, 'vel_output_sn30.txt')
 OUT_BESTFIT_FITS = os.path.join(DIR_PATH, PROC_PATH, PPXF_PROC_PATH, 'ppxf_bestfit_sn30.fits')
 OUT_ERROR_FITS = os.path.join(DIR_PATH, PROC_PATH, PPXF_PROC_PATH, 'ppxf_error_sn30.fits')
 DIR_SPEC = os.path.join(DIR_PATH, PROC_PATH, 'bin_spec_sn30')
 
-MIN_BINS = 50
+MIN_BINS = 23
+NUM_SMALL_FITS = 6
 
 
 def make_table():
@@ -77,11 +81,15 @@ def make_table():
         sci_data = sci_hdu[0].data
         sci_xaxis = sci_hdu[0].header[XAXIS_HEADER]
         sci_yaxis = sci_hdu[0].header[YAXIS_HEADER]
+        sci_xdim = sci_hdu[0].header[XDIM_HEADER]
+        sci_ydim = sci_hdu[0].header[XDIM_HEADER]
 
     with fits.open(VAR_EXT) as var_hdu:
         var_data = var_hdu[0].data
         var_xaxis = var_hdu[0].header[XAXIS_HEADER]
         var_yaxis = var_hdu[0].header[YAXIS_HEADER]
+        var_xdim = var_hdu[0].header[XDIM_HEADER]
+        var_ydim = var_hdu[0].header[XDIM_HEADER]
 
     assert sci_xaxis == var_xaxis, 'Sci and var planes have diffent x dimensions'
     assert sci_yaxis == var_yaxis, 'Sci and var planes have diffent y dimensions'
@@ -89,7 +97,10 @@ def make_table():
     with open(XYSN_FILE, 'w') as outfile:
         for i in range(sci_yaxis - 1):
             for j in range(sci_xaxis - 1):
-                outfile.write('   {}   {}   {}   {}\n'.format(i, j, sci_data[i, j], np.sqrt(var_data[i, j])))
+                # noise = np.sqrt(var_data[i, j] * var_xdim * var_ydim)
+                # outfile.write('   {}   {}   {}   {}\n'.format(i, j, sci_data[i, j] * sci_xdim * sci_ydim, noise))
+                noise = np.sqrt(var_data[i, j])
+                outfile.write('   {}   {}   {}   {}\n'.format(i, j, sci_data[i, j], noise))
 
 
 def voronoi_binning():
@@ -114,8 +125,8 @@ def voronoi_binning():
     # by this procedure. binNum uniquely specifies the bins and for this reason it is the only
     # number required for any subsequent calculation on the bins.
 
-    assert np.amax(np.transpose(binNum)) > MIN_BINS, 'Too few bins ({}) of expected at least {}'.format(np.amax(
-        np.transpose(binNum)), MIN_BINS)
+    # assert np.amax(np.transpose(binNum)) > MIN_BINS, 'Too few bins ({}) of expected at least {}'.format(np.amax(
+    # np.transpose(binNum)), MIN_BINS)
 
     np.savetxt(V2B_FILE, np.column_stack([x, y, binNum]), fmt=b'%10.6f %10.6f %8i')
     np.savetxt(V2B_XY_FILE, np.column_stack([xBar, yBar, xNode, yNode]), fmt=b'%10.6f %10.6f %10.6f %10.6f')
@@ -133,8 +144,7 @@ def bin_spectra():
     if not os.path.exists(DIR_VAR):
         os.mkdir(DIR_VAR)
 
-    v2b_output = pd.read_table(V2B_FILE, sep=r"\s*", engine='python',
-                               names=["x", "y", "bin"])  # , skiprows=1)
+    v2b_output = pd.read_table(V2B_FILE, sep=r"\s*", engine='python', names=["x", "y", "bin"])  # , skiprows=1)
     print('Highest bin number: ', np.amax(v2b_output['bin'].values))
 
     for i in range(np.amax(v2b_output['bin'].values) + 1):
@@ -170,22 +180,129 @@ def combine_spectra():
 
     import pyraf.iraf as iraf
 
-    v2b_output = pd.read_table(V2B_FILE, sep=r"\s*", engine='python',
-                               names=["x", "y", "bin"])  # , skiprows=1)
+    v2b_output = pd.read_table(V2B_FILE, sep=r"\s*", engine='python', names=["x", "y", "bin"])  # , skiprows=1)
 
-    for i in range(np.amax(v2b_output['bin'].values) + 1):
+    for i in range(12, 13):  # , np.amax(v2b_output['bin'].values) + 1):
         file_sci_i = FILE_SCI.format(i)
         file_var_i = FILE_VAR.format(i)
         bin_sci_i = BIN_SCI.format(i)
         bin_var_i = BIN_VAR.format(i)
 
-        assert os.path.exists("{}/{}".format(DIR_SCI, file_sci_i)), "File {}/{} does not exist".format(DIR_SCI,
-                                                                                                       file_sci_i)
-        assert os.path.exists("{}/{}".format(DIR_VAR, file_var_i)), "File {}/{} does not exist".format(DIR_VARa,
-                                                                                                       file_var_i)
+        assert os.path.exists(os.path.join(DIR_SCI, file_sci_i)), "File {}/{} does not exist".format(DIR_SCI,
+                                                                                                     file_sci_i)
+        assert os.path.exists(os.path.join(DIR_VAR, file_var_i)), "File {}/{} does not exist".format(DIR_VAR,
+                                                                                                     file_var_i)
+        # Check to see how many pixels are in this bin
+        spaxel_list = v2b_output.query('bin == {}'.format(i))
+        print('\nNumber of pixels in bin {}: {}'.format(i, len(spaxel_list)))
 
-        iraf.imcombine("@{}/{}".format(DIR_SCI, file_sci_i), "{}/{}".format(DIR_SCI_COMB, bin_sci_i), combine="sum")
-        iraf.imcombine("@{}/{}".format(DIR_VAR, file_var_i), "{}/{}".format(DIR_VAR_COMB, bin_var_i), combine="sum")
+        try:
+            iraf.imcombine("@{}/{}".format(DIR_SCI, file_sci_i), "{}/{}".format(DIR_SCI_COMB, bin_sci_i), combine="sum")
+            iraf.imcombine("@{}/{}".format(DIR_VAR, file_var_i), "{}/{}".format(DIR_VAR_COMB, bin_var_i), combine="sum")
+        except Exception, e:
+            print (e)
+            print('>>>>> WARNING: DOING MATH MYSELF <<<<<')
+            avoid_imexam_err(i, spaxel_list)
+
+
+def avoid_imexam_err(i, spaxel_list):
+    """
+    In order to avoid a 'floating point error' in imexamine I'm going to try and combine the fits sections as
+    numpy arrays
+    """
+
+    bin_sci_i = BIN_SCI.format(i)
+    bin_var_i = BIN_VAR.format(i)
+
+    # IMAGE_CUBE[int(line["x"].values[j - 1]) + 1, int(line["y"].values[j - 1]) + 1, *]
+
+    with fits.open(IMAGE_CUBE) as image_cube_hdu:
+        # Filename: dcsteqpxbprgN20051205S0006_add.fits
+        # No.    Name         Type      Cards   Dimensions   Format
+        # 0    PRIMARY     PrimaryHDU     214   ()
+        # 1    SCI         ImageHDU        68   (76, 49, 1300)   float32
+        # 2    VAR         ImageHDU        68   (76, 49, 1300)   float32
+        image_cube_sci = image_cube_hdu[1].data
+        image_cube_var = image_cube_hdu[2].data
+
+    # image_cube_sci_zero = np.zeros(image_cube_sci.shape)
+    # image_cube_var_zero = np.zeros(image_cube_var.shape)
+
+    imcomb_sci = []
+    imcomb_var = []
+
+    print(image_cube_sci.shape)
+    print(len(spaxel_list))
+
+    # image_cube_sci.shape = (1300, 49, 76) ie (z,y,x)
+    for k in range(image_cube_sci.shape[0]):
+        sum_sci = 0
+        sum_var = 0
+        for j in range(len(spaxel_list)):
+            sum_sci += image_cube_sci[k, spaxel_list["x"].values[j - 1] + 1, spaxel_list["y"].values[j - 1] + 1]
+            sum_var += image_cube_var[k, spaxel_list["x"].values[j - 1] + 1, spaxel_list["y"].values[j - 1] + 1]
+        imcomb_sci.append(sum_sci)
+        imcomb_var.append(sum_var)
+
+    image_cube_sci_hdu = fits.PrimaryHDU()
+    image_cube_sci_hdu.data = imcomb_sci
+    image_cube_sci_hdu.writeto("{}/{}".format(DIR_SCI_COMB, bin_sci_i), clobber=True)
+
+    image_cube_var_hdu = fits.PrimaryHDU()
+    image_cube_var_hdu.data = imcomb_var
+    image_cube_var_hdu.writeto("{}/{}".format(DIR_VAR_COMB, bin_var_i), clobber=True)
+
+
+def avoid_imexam_err2(i, spaxel_list):
+    """
+    In order to avoid a 'floating point error' in imexamine if you attempt to add too many files at once I split the
+    group into four parts, then add the four parts at the end.
+    """
+
+    import pyraf.iraf as iraf
+
+    file_sci_i = FILE_SCI.format(i)
+    file_var_i = FILE_VAR.format(i)
+    bin_sci_i = BIN_SCI.format(i)
+    bin_var_i = BIN_VAR.format(i)
+
+    spaxels = np.genfromtxt("{}/{}".format(DIR_SCI, file_sci_i), dtype='str')
+    spaxels_var = np.genfromtxt("{}/{}".format(DIR_VAR, file_var_i), dtype='str')
+
+    spaxels_files = []
+    spaxels_var_files = []
+
+    for j in range(0, NUM_SMALL_FITS):
+        (spaxels[j:(j + 1) * len(spaxel_list) / 4]).tofile(
+            "{}/tmp_{}".format(DIR_SCI, FILE_SCI.format('{}_{}'.format(i, j + 1))), sep='\n')
+        (spaxels_var[j:(j + 1) * len(spaxel_list) / 4]).tofile(
+            "{}/tmp_{}".format(DIR_VAR, FILE_VAR.format('{}_{}'.format(i, j + 1))), sep='\n')
+
+        spaxels_files.append("{}/tmp_{}[0]".format(DIR_SCI_COMB, BIN_SCI.format('{}_{}'.format(i, j + 1))))
+        spaxels_var_files.append("{}/tmp_{}[0]".format(DIR_VAR_COMB, BIN_VAR.format('{}_{}'.format(i, j + 1))))
+
+    (np.array(spaxels_files)).tofile("{}/tmp_{}".format(DIR_SCI, FILE_SCI.format('{}_list'.format(i))), sep='\n')
+    (np.array(spaxels_var_files)).tofile("{}/tmp_{}".format(DIR_VAR, FILE_VAR.format('{}_list'.format(i))), sep='\n')
+
+    for j in range(0, NUM_SMALL_FITS):
+        iraf.imcombine("@{}/tmp_{}".format(DIR_SCI, FILE_SCI.format('{}_{}'.format(i, j + 1))),
+                       "{}/tmp_{}".format(DIR_SCI_COMB, BIN_SCI.format('{}_{}'.format(i, j + 1))), combine="sum")
+        iraf.imcombine("@{}/tmp_{}".format(DIR_VAR, FILE_VAR.format('{}_{}'.format(i, j + 1))),
+                       "{}/tmp_{}".format(DIR_VAR_COMB, BIN_VAR.format('{}_{}'.format(i, j + 1))), combine="sum")
+
+    iraf.imcombine("@{}/tmp_{}".format(DIR_SCI, FILE_SCI.format('{}_list'.format(i))),
+                   "{}/{}".format(DIR_SCI_COMB, bin_sci_i), combine="sum")
+    iraf.imcombine("@{}/tmp_{}".format(DIR_VAR, FILE_VAR.format('{}_list'.format(i))),
+                   "{}/{}".format(DIR_VAR_COMB, bin_var_i), combine="sum")
+
+    for tmpfile in glob.glob('{}/tmp*'.format(DIR_SCI)):
+        os.remove(tmpfile)
+    for tmpfile in glob.glob('{}/tmp*'.format(DIR_VAR)):
+        os.remove(tmpfile)
+    for tmpfile in glob.glob('{}/tmp*'.format(DIR_SCI_COMB)):
+        os.remove(tmpfile)
+    for tmpfile in glob.glob('{}/tmp*'.format(DIR_VAR_COMB)):
+        os.remove(tmpfile)
 
 
 def ppxf_kinematics():
@@ -221,8 +338,8 @@ def ppxf_kinematics():
     # FWHM_gal = FWHM_gal/(1+z)   # Adjust resolution in Angstrom
 
     # galaxy, logLam1, velscale = util.log_rebin(lamRange1, gal_lin)
-    #   galaxy = galaxy/np.median(galaxy) # Normalize spectrum to avoid numerical issues
-    #   noise = galaxy*0 + 0.0049           # Assume constant noise per pixel here
+    # galaxy = galaxy/np.median(galaxy) # Normalize spectrum to avoid numerical issues
+    # noise = galaxy*0 + 0.0049           # Assume constant noise per pixel here
 
     galaxy, logLam1, velscale = util.log_rebin(LAM_RANGE, gal_lin)
     galaxy = galaxy / np.median(galaxy)  # Normalize spectrum to avoid numerical issues
@@ -231,21 +348,21 @@ def ppxf_kinematics():
     # of the library is included for this example with permission. See http://purl.org/cappellari/software
     # for suggestions of more up-to-date stellar libraries.
 
-    #   vazdekis = glob.glob(dir + 'Rbi1.30z*.fits')
-    #   vazdekis.sort()
-    #   FWHM_tem = 1.8 # Vazdekis spectra have a resolution FWHM of 1.8A.
+    # vazdekis = glob.glob(dir + 'Rbi1.30z*.fits')
+    # vazdekis.sort()
+    # FWHM_tem = 1.8 # Vazdekis spectra have a resolution FWHM of 1.8A.
 
     Mun = glob.glob(TEMPLATE_PATH + TEMPLATE_FITS)
 
     # Extract the wavelength range and logarithmically rebin one spectrum to the same velocity scale of the SAURON
     # galaxy spectrum, to determine the size needed for the array which will contain the template spectra.
 
-    #    hdu = fits.open(vazdekis[0])
-    #    ssp = hdu[0].data
-    #    h2 = hdu[0].header
-    #    lamRange2 = h2['CRVAL1'] + np.array([0.,h2['CDELT1']*(h2['NAXIS1']-1)])
-    #    sspNew, logLam2, velscale = util.log_rebin(lamRange2, ssp, velscale=velscale)
-    #    templates = np.empty((sspNew.size,len(vazdekis)))
+    # hdu = fits.open(vazdekis[0])
+    # ssp = hdu[0].data
+    # h2 = hdu[0].header
+    # lamRange2 = h2['CRVAL1'] + np.array([0.,h2['CDELT1']*(h2['NAXIS1']-1)])
+    # sspNew, logLam2, velscale = util.log_rebin(lamRange2, ssp, velscale=velscale)
+    # templates = np.empty((sspNew.size,len(vazdekis)))
 
     hdu = fits.open(Mun[0])
     ssp = hdu[0].data
@@ -271,14 +388,14 @@ def ppxf_kinematics():
     # Thus: The quadratic difference is sigma = sqrt(75^2 - 56.8^2) = 49.0 km/s
     # ---------------------------------------------------------------------------------
 
-    #    FWHM_dif = np.sqrt(FWHM_gal ** 2 - FWHM_tem ** 2)
-    #    sigma = FWHM_dif / 2.355 / h2['CDELT1']  # Sigma difference in pixels
+    # FWHM_dif = np.sqrt(FWHM_gal ** 2 - FWHM_tem ** 2)
+    # sigma = FWHM_dif / 2.355 / h2['CDELT1']  # Sigma difference in pixels
     sigma = 49.
 
     # Logarithmically rebin the whole Mun library of spectra, and store each template as a column in the array TEMPLATES,
 
-    #    for j in range(len(vazdekis)):
-    #        hdu = fits.open(vazdekis[j])
+    # for j in range(len(vazdekis)):
+    # hdu = fits.open(vazdekis[j])
     #        ssp = hdu[0].data
     #        ssp = ndimage.gaussian_filter1d(ssp, sigma)
     #        sspNew, logLam2, velscale = util.log_rebin(lamRange2, ssp, velscale=velscale)
@@ -370,9 +487,9 @@ def ppxf_kinematics():
 
 
 def make_overplot_data():
-    ###############
-    # NOT YET WORKING, REFER TO make_overplot_data.cl FOR A WORKING COPY
-    ###############
+    """
+    NOT YET WORKING, REFER TO make_overplot_data.cl FOR A WORKING COPY
+    """
 
     if not os.path.exists(DIR_SPEC):
         os.mkdir(DIR_SPEC)
@@ -394,6 +511,7 @@ def make_overplot_data():
         idl_proc / bin_spec_sn30 / cont_flux_ *.fits, fields = "mean", format = no > idl_proc / bin_spec_sn30 / binned_cont_flux.lis
     '''
 
+
 if __name__ == '__main__':
     """
     Follow the steps necessary to prepare input to pPXF, then run pPXF
@@ -407,14 +525,46 @@ if __name__ == '__main__':
         t = clock()
         voronoi_binning()
         print('Elapsed time: %.2f seconds' % (clock() - t))
-    if not os.path.exists(os.path.join(DIR_SCI, FILE_VAR.format(0))):
+    if not os.path.exists(os.path.join(DIR_SCI, FILE_VAR.format(1))):
         print('>>>>> Binning spectra')
         bin_spectra()
-    if not os.path.exists(os.path.join(DIR_SCI, BIN_VAR.format(0))):
+    if not os.path.exists(os.path.join(DIR_SCI, BIN_VAR.format(1))):
         print('>>>>> Combining spectra')
         combine_spectra()
+    '''
     if not os.path.exists(OUT_VEL_SIGMA):
         print('>>>>> pPXF')
         ppxf_kinematics()
+    '''
     print('>>>>> Done')
 
+""" At S/N 30 with James' reduction and cubing method
+41  initial bins.
+Reassign bad bins...
+26  good bins.
+Modified Lloyd algorithm...
+Iter:    1  Diff: 139.2
+Iter:    2  Diff: 13.32
+Iter:    3  Diff: 6.005
+Iter:    4  Diff: 3.506
+Iter:    5  Diff: 1.943
+Iter:    6  Diff: 1.511
+Iter:    7  Diff: 0.9909
+Iter:    8  Diff: 0.4755
+Iter:    9  Diff: 0.5045
+Iter:   10  Diff: 0.2212
+Iter:   11  Diff: 0.1989
+Iter:   12  Diff: 0.06859
+Iter:   13  Diff: 0.1068
+Iter:   14  Diff: 0.1636
+Iter:   15  Diff: 0.08666
+Iter:   16  Diff: 0.07709
+Iter:   17  Diff: 0.1417
+Iter:   18  Diff: 0.06984
+Iter:   19  Diff: 0.0317
+Iter:   20  Diff: 0.009915
+Iter:   21  Diff: 0
+20  iterations.
+Unbinned pixels:  0  /  3600
+Fractional S/N scatter (%): 13.3416729619
+"""
